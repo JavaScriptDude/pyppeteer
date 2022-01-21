170a0f4f7a9eda770d46f199d7fec3d5861684dd
JavaScriptDude <tim.c.quinn@gmail.com>
2022-01-21 15:24:09 -0500

    Restore To Original To Prevent Confusion

--- pyppeteer/__init__.py
+++ pyppeteer/__init__.py
@@ -24,8 +24,7 @@ except Exception:
 
 __chromium_revision__ = '588429'
 __base_puppeteer_version__ = 'v1.6.0'
-__pyppeteer_home__ = os.environ.get('PYPPETEER_HOME')  # type: str
-if not __pyppeteer_home__: __pyppeteer_home__ = AppDirs('pyppeteer').user_data_dir
+__pyppeteer_home__ = os.environ.get('PYPPETEER_HOME', AppDirs('pyppeteer').user_data_dir)  # type: str
 DEBUG = False
 
 from pyppeteer.launcher import connect, executablePath, launch, defaultArgs  # noqa: E402; noqa: E402
