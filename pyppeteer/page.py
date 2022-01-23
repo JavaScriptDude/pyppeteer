bcb2e5e67c5c2a7d96c5bc5947cbb6305e40988f
JavaScriptDude <tim.c.quinn@gmail.com>
2022-01-23 11:40:38 -0500

    Move _simplifyRemoteObject to Method of ConsoleMessage
    New method is getData(). Done to aid in consuming method from user code and tests.

--- pyppeteer/page.py
+++ pyppeteer/page.py
@@ -1767,8 +1767,7 @@ class ConsoleMessage(object):
             ts = f"{self.timestamp.strftime('%y%m%d-%H%M%S.%f')[:17]} " if isinstance(self.timestamp, datetime) else ""
 
             for jsh in self.args:
-                _ro = jsh._remoteObject
-                (_type, _subType, _data) = _simplifyRemoteObject(self.type, jsh._remoteObject)
+                (_type, _subType, _data) = self.getData(jsh)
 
                 # Try calling user defined dumper
                 _msg = self._call_dumper(dumper, _type, _subType, _data) if dumper else None
@@ -1798,6 +1797,54 @@ class ConsoleMessage(object):
             pass
         return "-"
 
+
+    # Serializes JSHandle._remoteObject and returns tuple of (_type, _subType, _data)
+    # For ones it can't figure out, it returns _subType of 'unknown' and remoteObject as _data
+    def getData(self, jsh: JSHandle) -> dict:
+        remoteObject: dict = jsh._remoteObject
+        _type = remoteObject.get('type', None)        
+        
+        if self.type == 'table':
+            _subType = remoteObject.get('className', None)
+            if _subType == 'Array' or _subType == 'Object':
+                _data = [] if _subType == 'Array' else {}
+                for _prop in remoteObject.get('preview', {}).get('properties', []):
+                    if _subType == 'Array':
+                        _data.append([_vpp.get('value', None) for _vpp in _prop.get('valuePreview', {}).get('properties', []) if 1])
+                    elif _subType == 'Object':
+                        _name = _prop.get('name', None)
+                        if not _name is None:
+                            _data[_name] = _prop.get('value', None)
+
+                return ('table', None, _data)
+
+
+        if _type == 'undefined': return ('undefined', None, None)
+        if _type == 'function' or _type == 'symbol': return (_type, None, remoteObject.get('description', "-"))
+
+        if _type == 'object':
+            _subType = remoteObject.get('subtype', '-')
+            if _subType == 'date':
+                return (_type, _subType, remoteObject.get('description', "-"))
+
+            elif _subType == 'null':
+                return ('null', None, None)
+                
+            else:
+                _className = remoteObject.get('className', '-')
+                return (_type, _className, remoteObject.get('preview', {}).get('properties', None))
+
+        else:
+            _data = remoteObject.get('value', None)
+            if _data is None: 
+                _data = remoteObject.get('unserializableValue', None)
+                if not _data is None: return (_type, "unserializable", _data)
+
+            if not _data is None: return (_type, None, _data)
+
+        return (_type, "unknown", remoteObject)
+
+
     def __repr__(self) -> str:
         return f"<ConsoleMessage> {self.toString()}"
 
@@ -1811,50 +1858,7 @@ def _getLocationFromStacktrace(stackTrace: dict) -> dict:
     }
 
 
-# Serializes type and data from remote object as tuple of (_type, _subType, _data)
-# For ones it can't figure out, it returns _subType of 'unknown' and remoteObject as _data
-def _simplifyRemoteObject(logType: str, remoteObject: dict) -> dict:
-    _type = remoteObject.get('type', None)
-    
-    if logType == 'table':
-        _subType = remoteObject.get('className', None)
-        if _subType == 'Array' or _subType == 'Object':
-            _data = [] if _subType == 'Array' else {}
-            for _prop in remoteObject.get('preview', {}).get('properties', []):
-                if _subType == 'Array':
-                    _data.append([_vpp.get('value', None) for _vpp in _prop.get('valuePreview', {}).get('properties', []) if 1])
-                elif _subType == 'Object':
-                    _name = _prop.get('name', None)
-                    if not _name is None:
-                        _data[_name] = _prop.get('value', None)
 
-            return ('table', None, _data)
-
-
-    if _type == 'undefined': return ('undefined', None, None)
-    if _type == 'function' or _type == 'symbol': return (_type, None, remoteObject.get('description', "-"))
-
-    if _type == 'object':
-        _subType = remoteObject.get('subtype', '-')
-        if _subType == 'date':
-            return (_type, _subType, remoteObject.get('description', "-"))
-
-        elif _subType == 'null':
-            return ('null', None, None)
-            
-        else:
-            _className = remoteObject.get('className', '-')
-            return (_type, _className, remoteObject.get('preview', {}).get('properties', None))
-
-    else:
-        _data = remoteObject.get('value', None)
-        if _data is None: 
-            _data = remoteObject.get('unserializableValue', None)
-            if not _data is None: return (_type, "unserializable", _data)
-
-        if not _data is None: return (_type, None, _data)
-
-    return (_type, "unknown", remoteObject)
 
 
 def _stringifyRemoteObject(_type: str, _subType: str, _data: Any) -> dict:
